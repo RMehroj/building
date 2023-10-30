e7ca833184292c93d195828c2ee2dbbb23433116
RMehroj <bekmirzayevmehroj@gmail.com>
2023-10-30 14:07:25 +0500

    add api

--- api/v1/mainpage/apps.py
+++ api/v1/mainpage/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MainpageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainpage'
+    name = 'api.v1.mainpage'
--- conf/settings.py
+++ conf/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api.v1.shop.apps.MainpageConfig',
 ]
 
 MIDDLEWARE = [
