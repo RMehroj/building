9b90ec9d56e4f0dcc44f7b81f9136bd47ef7a007
RMehroj <bekmirzayevmehroj@gmail.com>
2023-10-30 19:08:41 +0500

    models

--- api/v1/mainpage/models.py
+++ api/v1/mainpage/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
-# Create your models here.
+class Projects(models.Model):
+    uuid = models.UUIDField()
+    title = models.CharField()
+    apartment = models.IntegerField()
+    parking = models.IntegerField()
+    price = models.ImageField()
+    video = models.FileField()
+    text = models.TextField()
+    start_date = models.DateField()
+    finsh_date = models.DateField()
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+
+class SocialNetwork(models.Model):
+    title = models.CharField()
+    instagram = models.URLField()
+    telegram = models.URLField()
+    facebook = models.URLField()
+
+
+class SalesOffice(models.Model):
+    uuid = models.UUIDField()
+    project_title = models.ForeignKey(Projects)
+    social_network = models.ForeignKey(SocialNetwork)
+    phone = models.Phonenumber()
+    location = PlainLocationField()
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+
+class Newsroom(models.Model):
+    uuid = models.UUIDField()
+    title = models.CharField()
+    text = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+
+class About(models.Model):
+    uuid = models.UUIDField()
+    company_name = models.CharField()
+    license = models.ImageField()
+    achievements = models.ImageField()
+    phone = models.Phonenumber()
+    about = models.TextField()
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
