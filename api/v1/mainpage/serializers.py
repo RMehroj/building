9808a59821c41b075d3c2eff357c3988da8a3500
RMehroj <bekmirzayevmehroj@gmail.com>
2023-11-01 12:31:40 +0500

    serializer update

--- api/v1/mainpage/serializers.py
+++ api/v1/mainpage/serializers.py
@@ -1,15 +1,99 @@
 from rest_framework import serializers
-from .models import Apartment, Projects
+
+from conf.serializers import DynamicFieldsModelSerializer
+from . import models
 
 
 class ApartmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Apartment
-        fields = ['uuid', 'apartment', 'floor', 'parking', 'created_at', 'update_at']
+        model = models.Apartment
+        fields = [
+            'uuid',
+            'apartment',
+            'floor',
+            'parking',
+            'created_at',
+            'update_at',
+            ]
 
 
-class ProjectsSerializer(serializers.ModelSerializer):
+class ProjectsSerializer(DynamicFieldsModelSerializer):
+    apartments = ApartmentSerializer(
+        many=True, 
+        read_only=True
+        )
+    
     class Meta:
-        model = Projects
-        fields = ['title', 'apartments', 'image', 'description', 'finish_date', 'created_at', 'update_at']
+        model = models.Projects
+        fields = [
+            'uuid',
+            'title',
+            'apartments',
+            'image',
+            'description',
+            'finish_date',
+            'created_at',
+            'update_at',
+            ]
 
+
+class SocialnetworkSerializer(DynamicFieldsModelSerializer):
+    class Meta:
+        model = models.SocialNetwork
+        fields = [
+            'title',
+            'instagram',
+            'telegram',
+            'facebook',
+        ]
+
+
+class SalesOfficeSerializer(DynamicFieldsModelSerializer):
+    projects = ProjectsSerializer(
+        read_only=True,
+        fields=["projects"],
+    )
+    social_network = SocialnetworkSerializer(
+        read_only=True,
+        fields=["social_network"],
+    )
+    class Meta:
+        model = models.SalesOffice
+        fields = [
+            'uuid',
+            'projects',
+            'social_network'
+            'phone',
+            'city',
+            'location',
+            'created_at',
+            'update_at',
+        ]
+
+
+class NewsroomSerializer(serializers.Serializer):
+    class Meta:
+        model = models.Newsroom
+        fields = [
+            'uuid',
+            'title',
+            'description',
+            'image',
+            'created_at',
+            'update_at',
+        ]
+
+
+class AboutSerializer(serializers.Serializer):
+    class Meta:
+        model = models.About
+        fields = [
+            'uuid',
+            'company_name',
+            'license',
+            'achievements',
+            'phone',
+            'descrption',
+            'created_at',
+            'update_at',
+        ]
--- api/v1/mainpage/views.py
+++ api/v1/mainpage/views.py
@@ -2,17 +2,21 @@ from rest_framework import generics
 from .models import Apartment, Projects
 from .serializers import ApartmentSerializer, ProjectsSerializer
 
+
 class ApartmentListCreateView(generics.ListCreateAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
+
 
 class ApartmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
 
+
 class ProjectsListCreateView(generics.ListCreateAPIView):
     queryset = Projects.objects.all()
     serializer_class = ProjectsSerializer
+
 
 class ProjectsDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Projects.objects.all()
